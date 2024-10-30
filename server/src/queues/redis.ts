ca33939ab9085f0a6a0b01e67c78b52752cc4177
RajVerma97 <rajneeskumar2545@gmail.com>
2024-10-31 02:42:22 +0530

    add redis host and redis port

--- server/src/queues/redis.ts
+++ server/src/queues/redis.ts
@@ -1,8 +1,16 @@
+import { configDotenv } from 'dotenv';
 import { Redis } from 'ioredis';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const redisHost = process.env.REDIS_HOST ;
+const redisPort = process.env.REDIS_PORT
+  ? parseInt(process.env.REDIS_PORT, 10)
+  : 6379;
 
 const redisConnection = new Redis({
-  host: 'localhost',
-  port: 6379,
+  host: redisHost,
+  port: redisPort,
   maxRetriesPerRequest: null,
 });
 
