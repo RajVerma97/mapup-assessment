a53db90eed392520defa14ec1eb5ca58e35a30e8
RajVerma97 <rajneeskumar2545@gmail.com>
2024-10-31 14:59:50 +0530

    put error handling env logs in redis.ts

--- server/src/queues/redis.ts
+++ server/src/queues/redis.ts
@@ -3,14 +3,27 @@ import { Redis } from 'ioredis';
 import dotenv from 'dotenv';
 dotenv.config();
 
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.prod' });
+} else {
+  dotenv.config({ path: '.env.local' });
+}
+
 const redisHost = process.env.REDISHOST;
 const redisPort = process.env.REDISPORT
   ? parseInt(process.env.REDISPORT, 10)
   : 6379;
 
-const redisPassword = process.env.REDISPASSWORD || undefined;
-console.log(` holaa!Connecting to Redis at ${redisHost}:${redisPort}`);
+const redisPassword =
+  process.env.NODE_ENV === 'production' ? process.env.REDISPASSWORD : undefined;
 
+if (!redisHost) {
+  throw new Error('REDISHOST VARIABLE IS NOT DEFINED');
+}
+
+if (!redisPort) {
+  throw new Error('REDISPORT VARIABLE IS NOT DEFINED');
+}
 
 const redisConnection = new Redis({
   host: redisHost,
@@ -18,5 +31,10 @@ const redisConnection = new Redis({
   password: redisPassword,
   maxRetriesPerRequest: null,
 });
+
+console.log(
+  `Connecting to Redis at ${redisHost}:${redisPort}` +
+    (redisPassword ? ' with password' : ' without password')
+);
 
 export default redisConnection;
